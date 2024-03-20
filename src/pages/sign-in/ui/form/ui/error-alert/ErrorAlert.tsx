3c6cfa0996462c63a0220a1f63b15c6f82c4cead
kasapvictor <kasap.victor@gmail.com>
2024-03-19 19:30:20 -0700

    feat: Added Sign-In form

--- src/pages/sign-in/ui/form/ui/error-alert/ErrorAlert.tsx
+++ src/pages/sign-in/ui/form/ui/error-alert/ErrorAlert.tsx
@@ -7,7 +7,7 @@ export const ErrorAlert = ({ error }: { error: ApiError | null }) => {
 
   return (
     <div
-      className="p-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+      className="p-4 mb-5 text-sm text-red-800 dark:text-red-900 rounded-lg bg-red-50 dark:text-red-400 dark:bg-red-200"
       role="alert"
     >
       <span className="font-medium">Error:</span>{" "}
--- src/pages/sign-in/ui/form/ui/input-user-name/InputUserName.tsx
+++ src/pages/sign-in/ui/form/ui/input-user-name/InputUserName.tsx
@@ -13,19 +13,19 @@ export const InputUserName = ({
     <div>
       <label
         htmlFor="username"
-        className="block text-sm font-medium leading-6 text-gray-900"
+        className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-400"
       >
         Login Demo: <code>kminchelle</code>
       </label>
       <div className="relative mt-2 rounded-md shadow-sm">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <span className="text-gray-500 sm:text-sm">⌘</span>
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 ">
+          <span className="text-gray-500 sm:text-sm dark:text-gray-300">⌘</span>
         </div>
         <input
           type="text"
           name="username"
           id="username"
-          className="block w-full rounded-md border-0 py-1.5 pl-8 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 py-1.5 pl-8 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:text-gray-300"
           placeholder="johnsnow1"
           value={username}
           onChange={onChange}
