b2d8956c49bbe2160d7fc1c428484fa0ff4a971e
kasapvictor <kasap.victor@gmail.com>
2024-03-21 15:41:02 -0700

    feat: Added queryClient as Router context2

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -13,22 +13,22 @@ type RouterContext = {
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootRoute,
   errorComponent: ({ error }) => <>Error: {(error as ApiError).message}</>,
-  pendingComponent: () => <h4>Checking Authorization...</h4>,
+  pendingComponent: () => <h1>Checking Authorization...</h1>,
 
   // loader: ({ context: { queryClient } }) =>
   //   queryClient.ensureQueryData(userQueryOptions()),
 
   loader: ({ context: { queryClient } }) => {
     try {
-      // * return нужен чтобы отобразить ошибку в errorComponent
-      // * если не сделать return то errorComponent не будет отображен
+      // * return нужен чтобы отобразить ошибку в errorComponent || pendingComponent
+      // * если не сделать return то errorComponent || pendingComponent не будет отображен
       // return queryClient.ensureQueryData(userQueryOptions());
-      return queryClient.ensureQueryData(userQueryOptions());
+      queryClient.ensureQueryData(userQueryOptions());
     } catch (err) {
       console.log("[ROOT ERROR]:", err);
     }
 
-    // * Альтернативный вариант использования
+    // * Альтернативный вариант получения данных
     // return queryClient.ensureQueryData(userQueryOptions()).catch((err) => {
     //   console.log("[ROOT ERROR]:", err);
     // });
